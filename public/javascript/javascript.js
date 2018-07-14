ae29f195af3851f0a977448995f1c3a49a20a252
CuartoHokage <jaime199505@hotmail.com>
2018-07-14 11:02:27 -0500

    Finalizada la parte de generar los números aleatorios javascript.js

---
+++ public/javascript/javascript.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    //valores estaticos para numeros aleatoreos
+    
+    $('#btn-generar_tabla').click(function(){
+        var a,c,x,m,rn=0,contador1=0;
+    a=81;
+    c=89;
+    x=11;
+    m=100;
+    //var paso1=0;
+    //var resultado=0;
+    
+    var demanda= $('#demanda_txt').val();
+    var cantidadAleatorios=demanda;    
+        var contador= 0;
+        var htmlpuro='<table class="table table-striped"><thead><tr><th>Demanda</th><th>Aleatorio</th><th>P. Frecuencia</th><th>P. Acumulada</th><th>Indice menor</th><th>Indice mayor</th></tr></thead><tbody>';
+        
+        var eventos= $('eventos_txt').val();
+        var probabilidad= $('probabilidad_txt').val();
+        while((contador++)<demanda){
+            var paso1=a*x;
+            var resultado= ((paso1+c)/m).toFixed(2);
+             resultado= (resultado- Math.trunc(resultado)).toFixed(2);
+             resultado= resultado*100;
+             resultado= Math.trunc(resultado);
+             x=resultado;
+             rn= resultado/m;
+            htmlpuro+='<tr>';
+                htmlpuro+= '<td>'+contador+'</td>';
+                htmlpuro+= '<td>'+rn+'</td>';
+                htmlpuro+= '<td>'+contador+'</td>';
+                htmlpuro+= '<td>'+contador+'</td>';
+                htmlpuro+= '<td>'+contador+'</td>';
+                htmlpuro+= '<td>'+contador+'</td>';
+            htmlpuro+='<tr>';
+        }
+        htmlpuro+='</tbody></table>';
+        $('#table-responsive').html(htmlpuro)
+    });
+});
