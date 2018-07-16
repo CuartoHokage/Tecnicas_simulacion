db1bed9d4525710a770f4ae47894e1f302a577cb
CuartoHokage <jaime199505@hotmail.com>
2018-07-15 19:56:30 -0500

    sdsds

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 3000
 	//referencia a la ubicación de la base de datos 
 	// db: process.env.MONGODB || 'mongodb://localhost:27017/tecnicas_simulacion',
-	SECRET_TOKEN: 'miclavedetokens'
+	//SECRET_TOKEN: 'miclavedetokens'
 }
